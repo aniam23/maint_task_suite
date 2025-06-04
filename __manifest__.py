a262b0bde68f1710dbdfe35adb93a823a9dc7ed1
aniam23 <developer2@horizontrailers.com>
2025-06-04 09:28:28 -0600

    nuevo repositorio para desarrollo mantenimiento

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Task Maintenance Suite",
+
+    'summary': """
+        Task Maintenance Suite""",
+
+    'description': """
+       Automated Task Management Suite for Odoo
+    """,  
+    'author': "Anilu Amado Aguero",
+    'website': "http://www.horizontrailers.com",
+    'sequence': 3,
+    'category': 'All',
+
+    'version': '1.0',
+
+    'depends': ['base','maintenance'], 
+    'data': [
+        'security/ir.model.access.csv',
+         'views/task_maint_suite.xml',
+    ],
+    
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+    
+}
---
+++ models/task_maint_suite.py
@@ -0,0 +1,10 @@
+from dateutil.relativedelta import relativedelta
+from odoo import models, fields, api
+from datetime import date
+from odoo.fields import Date
+from odoo.exceptions import UserError  
+from odoo.exceptions import ValidationError
+
+class MaintenanceRequest(models.Model):  
+    _inherit = 'maintenance.request'
+   
